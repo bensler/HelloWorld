d26ec6247152cd316528e17b256b4599e75e2cea
Thomas Bensler <bensler@web.de>
2019-06-29 14:30:24 +0200

    jetzt mit if-else

--- src/com/bensler/helloworld/HelloWorld.java
+++ src/com/bensler/helloworld/HelloWorld.java
@@ -13,18 +13,25 @@ public class HelloWorld {
 	}
 	
 	public static void zwei(String[] bernd) {
+		System.out.println(bernd.length + " arguments");
 		drei(bernd, 0);
 		drei(bernd, 1);
 		drei(bernd, 2);
 	}
 	
 	public static void drei(String[] args, int index) {
-		System.out.println("Hello World! " + args[index]);
+		if (index < args.length) {
+			System.out.println("Hello World! " + args[index]);
+		} else {
+			System.out.println("Hello World! no more arguments");
+		}
 	}
 	
 	
 	public static int plus(int summand1, int summand2) {
-		return summand1 + summand2;
+		int summe = summand1 + summand2;
+		
+		return summe;
 	}
 	
 }
